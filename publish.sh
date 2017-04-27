d42a46058eae147f34db3bc5dabc96367c78b7e9
Jean-Philippe Ouellet <jpo@vt.edu>
2017-04-27 15:21:47 -0400

    Fix publish script
    Hard-coded remotes are eww, but it's better than having to re-init
    the repo all the time.
    
    Could be improved, but higher priorities...
    
    Also backgrounding rm was a typo

--- publish.sh
+++ publish.sh
@@ -3,9 +3,12 @@
 set -e
 
 cd $(dirname "$0")
-rm -rf build/&
+rm -rf build/
 npm run build
 cd build
+git init
+git remote add origin gh:jpouellet/dor-sim
+git checkout -b gh-pages
 git add -A
 git commit -m 'Publish'
-git push -f
+git push -fu origin gh-pages
